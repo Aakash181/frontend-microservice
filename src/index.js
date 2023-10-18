adaedc03b4309808b7ce23caffe029bb855fd8f4
Aakash181 <20bcs001@iiitdwd.ac.in>
2023-10-18 17:42:07 +0530

    set as env

--- src/index.js
+++ src/index.js
@@ -43,8 +43,8 @@ const connection = mysql.createConnection({
     host: process.env.MYSQL_HOST || "mysql-service.default.svc",
     // host: process.env.MYSQL_HOST || "localhost",
     user: process.env.MYSQL_USER || "root",
-    password: process.env.MYSQL_PASSWORD || "YOUR-PASSWORD",
-    database: process.env.MYSQL_DATABASE || "demodb"
+    password: process.env.MYSQL_PASSWORD ,
+    database: process.env.MYSQL_DATABASE 
 });
 
 connection.connect((err) => {
